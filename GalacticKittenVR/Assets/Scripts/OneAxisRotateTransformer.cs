a6040952ca4bac297039cc2aaa9cd2c2ab1de41e
Yash Sadhukhan <yskhan61@gmail.com>
2023-11-20 17:37:32 +0530

    Added Left Throttle Handle, and Right Control Handle Updated

--- GalacticKittenVR/Assets/Scripts/OneAxisRotateTransformer.cs
+++ GalacticKittenVR/Assets/Scripts/OneAxisRotateTransformer.cs
@@ -25,13 +25,25 @@ namespace GalacticKittenVR.Spaceship
         private Transform _visualTransform;
 
         [SerializeField, Tooltip("The axis of rotation of the visual transform")]
-        private Axis _axis = Axis.Right;
+        private Axis _axisOfRotation = Axis.Right;
+
+        [SerializeField, Tooltip("The axis of visual transform that need to be oriented to the grabber")]
+        private Axis _axisToOrient = Axis.Up;
+
+        [SerializeField, Tooltip("The max angle between visual transform's axis of orientation and pivot transform's Y axis")]
+        private int _angleConstraint = 10;
 
         private IGrabbable _grabbable;
+        private Vector3 _localAxisOfRotationOfPivotTransform;
+        private Vector3 _localAxisOfVisualTransformToOrient;
 
         public void Initialize(IGrabbable grabbable)
         {
             _grabbable = grabbable;
+            _localAxisOfRotationOfPivotTransform = Vector3.zero;
+            _localAxisOfRotationOfPivotTransform[(int)_axisOfRotation] = 1;
+            _localAxisOfVisualTransformToOrient = Vector3.zero;
+            _localAxisOfVisualTransformToOrient[(int)_axisToOrient] = 1;
         }
 
         public void BeginTransform()
@@ -41,7 +53,28 @@ namespace GalacticKittenVR.Spaceship
 
         public void UpdateTransform()
         {
-            
+            Vector3 vectorFromPivotToGrabberInWorldSpace =
+                _grabbable.GrabPoints[0].position - _pivotTransform.position;
+
+            Vector3 axisOfRotation = _pivotTransform.TransformDirection(_localAxisOfRotationOfPivotTransform);
+            Vector3 axisToOrient = _visualTransform.TransformDirection(_localAxisOfVisualTransformToOrient);
+
+            Vector3 projectedVectorFromPivotToGrabberInWorldSpaceAlongPlaneWithNormalOfAxisToOrient =
+                Vector3.ProjectOnPlane(vectorFromPivotToGrabberInWorldSpace, axisOfRotation);
+
+            float angleBetweenVectorFromPivotToGrabberInWorldSpaceAndPivotTransformYAxis =
+                Vector3.Angle(projectedVectorFromPivotToGrabberInWorldSpaceAlongPlaneWithNormalOfAxisToOrient, _pivotTransform.up);
+
+            if (angleBetweenVectorFromPivotToGrabberInWorldSpaceAndPivotTransformYAxis > _angleConstraint)
+            {
+                return;
+            }
+
+            _visualTransform.rotation =
+                Quaternion.FromToRotation(
+                    axisToOrient,
+                    projectedVectorFromPivotToGrabberInWorldSpaceAlongPlaneWithNormalOfAxisToOrient) * _visualTransform.rotation;
+
         }
 
         public void EndTransform()

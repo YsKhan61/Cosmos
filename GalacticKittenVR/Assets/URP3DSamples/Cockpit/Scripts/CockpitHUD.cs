2fdc9f739892b4a9d3b68dfa4668cd7f6b36201b
Yash Sadhukhan <yskhan61@gmail.com>
2023-11-24 16:03:30 +0530

    SpaceShipUI to indicate the orientation of the spaceship

--- GalacticKittenVR/Assets/URP3DSamples/Cockpit/Scripts/CockpitHUD.cs
+++ GalacticKittenVR/Assets/URP3DSamples/Cockpit/Scripts/CockpitHUD.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
 
+// [ExecuteAlways]
 public class CockpitHUD : MonoBehaviour
 {
     
     [Header("Fighter Ship")]
     public Transform fighterShip;
-    
+
+    private void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
         fighterShip.forward = Vector3.forward;
     }
+
+    // Use UniTask to run the update loop in a thread
+    // void Start()
+    // {
+    //     UniTask.Run(() =>
+    //     {
+    //         while (true)
+    //         {
+    //             fighterShip.forward = Vector3.forward;
+    //         }
+    //     });
+    // }
+
+
 }

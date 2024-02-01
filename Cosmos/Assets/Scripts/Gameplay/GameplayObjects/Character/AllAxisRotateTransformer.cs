df1435bc2460a246b439354713d7fa5b701a7e48
Yash Sadhukhan <yskhan61@gmail.com>
2024-02-01 11:53:23 +0530

    Created All Axis Rotate Transformer and updated TouchScreen UI

---
+++ Cosmos/Assets/Scripts/Gameplay/GameplayObjects/Character/AllAxisRotateTransformer.cs
@@ -0,0 +1,92 @@
+
+using Cosmos.Utilities;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Cosmos.Gameplay.GameplayObjects.Character
+{
+    /// <summary>
+    /// Used to rotate a transform on pivot's X Y Z axis based on RotateInput;
+    /// </summary>
+    public class AllAxisRotateTransformer : MonoBehaviour, IControlHandle
+    {
+        [SerializeField, Tooltip("The data based on which the visual will rotate")] 
+        private Vector3DataSO _rotateInput;
+
+        [SerializeField, Tooltip("This will be the pivot relative to calculate orientations of handle")]
+        private Transform _pivotTransform;
+        public Transform PivotTransform => _pivotTransform;
+
+        [SerializeField, Tooltip("This will be the visual that will be oriented")]
+        private Transform _visualTransform;
+
+        [SerializeField, Tooltip("The sensitivity of throttle")]
+        private float _sensitivity = 1;
+
+        [SerializeField, Tooltip("The max angle between visual transform's axis of orientation and pivot transform's Y axis")] 
+        private int _angleConstraint = 10;
+        public int AngleConstraint => _angleConstraint;
+
+        private Quaternion _initialVisualLocalRotation;
+
+        private float _relativeAngleX;
+        private float _relativeAngleY;
+        private float _relativeAngleZ;
+
+        private void Awake()
+        {
+            _initialVisualLocalRotation = Quaternion.Inverse(_pivotTransform.rotation) * _visualTransform.rotation;
+
+            _relativeAngleX = 0;
+            _relativeAngleZ = 0;
+        }
+
+        private void Update()
+        {
+            if (_rotateInput.value != Vector3.zero)
+            {
+                ManualControl();
+            }
+            else
+            {
+                AutoReset();
+            }
+        }
+
+        private void ManualControl()
+        {
+            Debug.Log($"_rotateInputValue: {_rotateInput.value}");
+
+            _relativeAngleX = Mathf.Lerp(-_angleConstraint, _angleConstraint, Mathf.InverseLerp(1, -1, _rotateInput.value.x));
+            _relativeAngleY = Mathf.Lerp(-_angleConstraint, _angleConstraint, Mathf.InverseLerp(1, -1, _rotateInput.value.y));
+            _relativeAngleZ = Mathf.Lerp(-_angleConstraint, _angleConstraint, Mathf.InverseLerp(1, -1, _rotateInput.value.y));
+
+            Debug.Log($"_relativeAngleX: {_relativeAngleX}, _relativeAngleZ: {_relativeAngleZ}");
+
+            Quaternion inputRotationInPivotSpace = Quaternion.Euler(_relativeAngleX, _relativeAngleY, _relativeAngleZ);
+
+            _visualTransform.localRotation = Quaternion.Slerp(
+                               _visualTransform.localRotation,
+                                inputRotationInPivotSpace * _initialVisualLocalRotation,
+                                5f* Time.deltaTime);
+        }
+
+        private void AutoReset()
+        {
+            _visualTransform.rotation = Quaternion.Slerp(
+                    _visualTransform.rotation,
+                    _pivotTransform.rotation * _initialVisualLocalRotation,
+                    _sensitivity * Time.deltaTime);
+
+            _relativeAngleX = _visualTransform.localEulerAngles.x;
+            if (_relativeAngleX > 180)
+                _relativeAngleX -= 360;
+
+
+            _relativeAngleZ = _visualTransform.localEulerAngles.z;
+            if (_relativeAngleZ > 180)
+                _relativeAngleZ -= 360;
+        }
+    }
+
+}
--- Cosmos/Assets/Scripts/Gameplay/GameplayObjects/Character/TouchScreen/ControlHandleInputValueFromMobileJoystick.cs
+++ Cosmos/Assets/Scripts/Gameplay/GameplayObjects/Character/TouchScreen/ControlHandleInputValueFromMobileJoystick.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 namespace Cosmos.Gameplay.GameplayObjects.Character
 {
+    /// <summary>
+    /// Gets the input value from the touch screen joysticks (pitch, yaw, roll) and stores it in a Vector3DataSO.
+    /// </summary>
     public class ControlHandleInputValueFromMobileJoystick : MonoBehaviour
     {
-        [SerializeField] private VariableJoystick _joystick;
+        [SerializeField] private VariableJoystick _pitchStick;
+        [SerializeField] private VariableJoystick _yawStick;
+        [SerializeField] private VariableJoystick _rollStick;
+        
 
-        [SerializeField] private Vector2DataSO _controlHandleInputData;
+        [SerializeField] private Vector3DataSO _controlHandleInputData;
 
         private void Update()
         {
-            _controlHandleInputData.value = _joystick.Direction;
+            _controlHandleInputData.value = new Vector3(_pitchStick.Horizontal, _yawStick.Horizontal, _rollStick.Horizontal);
         }
     }
 }
